e92e7d910eecd7522eb2613e962e2e9064c858ff
jessexknight <jesse.x.knight@gmail.com>
2019-07-21 16:38:44 -0400

    move ci files around, update docs with badges

--- ci/pylint/get-badge.sh
+++ ci/pylint/get-badge.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-score=$(pylint ndna/ | tail -n2 | awk '{print $7}' | cut -d"/" -f1)
+score=$(pylint --rcfile=ci/pylint/.pylintrc ndna/ | tail -n2 | awk '{print $7}' | cut -d"/" -f1)
 url=https://mperlet.github.io/pybadge/badges/$score.svg
-curl -s $url > ci/pylint/badge.svg 
+curl -s $url > ci/pylint/badge.svg
+cp ci/pylint/badge.svg docs/src/_static/icon/pylint-badge.svg
