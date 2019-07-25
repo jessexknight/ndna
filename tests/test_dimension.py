5fc8258bb6093fab4fc4a6840c83fd1f202f8d6b
jessexknight <jesse.x.knight@gmail.com>
2019-07-25 18:15:31 -0400

    small clean-ups

--- tests/test_dimension.py
+++ tests/test_dimension.py
@@ -1,10 +1,11 @@
+import os
 import pytest
 from ndna.space import Dimension,Space
 from ndna.utils import loadjson
 
 dims = {
   key: Dimension(**spec)
-  for key,spec in loadjson('tests/data/dimensions.json').items()
+  for key,spec in loadjson(os.path.join('tests','data','dimensions.json')).items()
 }
 space = Space(dims.values())
 
