663ce7154ea82c5aed764e521f4ec7beaf550b5a
jessexknight <jesse.x.knight@gmail.com>
2019-09-24 16:18:17 -0400

    add Selector class, update testing results

--- tests/data.py
+++ tests/data.py
@@ -1,14 +1,24 @@
 import os
 from ndna.space import Dimension,Space,Array
+from ndna.ops import Selector
 from ndna.io import loadjson,odict
 
+# Dimension
 dims = odict([
   (key, Dimension(**spec))
   for key,spec in loadjson(os.path.join('tests','data','dimensions.json')).items()
 ])
+# Space
 space = Space(dims.values())
-X    = Array(0, space=space, keys=[])
-Xi   = Array([1,2,3], space=space, keys=['i'])
-Xk   = Array([1,2], space=space, keys=['k'])
+# Array
+X    = Array(0,                 space=space, keys=[])
+Xi   = Array([1,2,3],           space=space, keys=['i'])
+Xk   = Array([1,2],             space=space, keys=['k'])
 Xik  = Array([[1,2,3],[4,5,6]], space=space, keys=['i','k'])
-Xijk = Array(range(3*7*2), space=space, keys=['i','j','k'])
+Xijk = Array(range(3*7*2),      space=space, keys=['i','j','k'])
+# Selector
+si  = Selector('si',    space=space, i='high')
+sio = Selector('sio',   space=space, i='high', memory=False)
+sk  = Selector('sk',    space=space, k='female')
+sik = Selector('si sk', space=space, i='high',k='female')
+sj3 = Selector('sj2',   space=space, j=[10,20,30])
--- tests/test_array_ops.py
+++ tests/test_array_ops.py
@@ -1,5 +1,10 @@
 import pytest
-from . import data
+import numpy as np
+import copy
+from ndna.ops import Selector
+from tests import data
+
+# TODO: performance benchmarking vs np.ndarray as another badge
 
 def test_array_ops():
   # Array.__add__
@@ -18,4 +23,25 @@ def test_array_ops():
   assert (data.Xik / 1).keys == ['i','k']
   assert (1 / data.Xik).keys == ['i','k']
   assert (data.Xi / data.Xk).keys == ['i','k']
-  
+
+def test_selector():
+  csio = copy.deepcopy(data.sio)
+  csio.register(data.Xik.shape)
+  # Selector.__str__
+  assert str(data.si) == '< Selector "si" {\'i\': \'high\'} >'
+  # Selector.__repr__
+  assert repr(data.si) == '{\'i\': \'high\'}'
+  # Selector.__call__
+  with pytest.raises(AttributeError,match='has no attribute \'shape\''):
+    data.si(None)
+  assert np.all(data.si(data.Xik) == np.array([[[1,2]]]))
+  assert data.Xik.shape in data.si.pre
+  assert np.all(data.si(data.Xik) == np.array([[[1,2]]]))
+  assert np.all(data.sio(data.Xik) == np.array([[[1,2]]]))
+  assert data.Xik.shape not in data.sio.pre
+  # Selector.register
+  assert data.Xik.shape in csio.pre
+  # Selector.merge
+  assert data.si.merge(data.sk) == data.sik
+  # complex operations
+  assert data.Xijk[data.sj3.merge(data.si)].shape == (1,3,2)
